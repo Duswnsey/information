8167d2dec5d8787f94ba59bd0d00b94c7c40c596
Duswnsey <157892378+Duswnsey@users.noreply.github.com>
2024-07-19 12:44:03 +0900

    Update tmp+chroot.sh

--- tmp+chroot.sh
+++ tmp+chroot.sh
@@ -1,14 +1,17 @@
+# Please, run this script as builder user!!!
+# How to make builder user:
+# https://www.linuxfromscratch.org/lfs/view/systemd/chapter04/addinguser.html
 # set variables
-INSTALL_DIR="/mnt"
+ISDIR="/mnt/ynjn"
 
 # binutils
 tar -xf binutils-2.42.tar.xz
 cd binutils-2.42
 mkdir -v build
 cd       build
-../configure --prefix=$LFS/tools \
-             --with-sysroot=$LFS \
-             --target=$LFS_TGT   \
+../configure --prefix=$ISDIR/tools \
+             --with-sysroot=$ISDIR\
+             --target=$(uname -m)-ynjun-linux-gnu   \
              --disable-nls       \
              --enable-gprofng=no \
              --disable-werror    \
@@ -37,10 +40,10 @@ esac
 mkdir -v build
 cd       build
 ../configure                  \
-    --target=$LFS_TGT         \
-    --prefix=$LFS/tools       \
+    --target=$(uname -m)-ynjun-linux-gnu         \
+    --prefix=$ISDIR/tools       \
     --with-glibc-version=2.39 \
-    --with-sysroot=$LFS       \
+    --with-sysroot=$ISDIR      \
     --with-newlib             \
     --without-headers         \
     --enable-default-pie      \
@@ -60,7 +63,7 @@ make
 make install
 cd ..
 cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+  `dirname $($(uname -m)-ynjun-linux-gnu-gcc -print-libgcc-file-name)`/include/limits.h
 cd ..
 rm -rf gcc-14.1.0
 
@@ -70,7 +73,7 @@ cd linux-6.9.9
 make mrproper
 make headers
 find usr/include -type f ! -name '*.h' -delete
-cp -rv usr/include $LFS/usr
+cp -rv usr/include $ISDIR/usr
 cd ..
 rm -rf linux-6.9.9
 
@@ -78,10 +81,10 @@ rm -rf linux-6.9.9
 tar -xf glibc-2.39.tar.xz
 cd glibc-2.39
 case $(uname -m) in
-    i?86)   ln -sfv ld-linux.so.2 $LFS/lib/ld-lsb.so.3
+    i?86)   ln -sfv ld-linux.so.2 $ISDIR/lib/ld-lsb.so.3
     ;;
-    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64
-            ln -sfv ../lib/ld-linux-x86-64.so.2 $LFS/lib64/ld-lsb-x86-64.so.3
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $ISDIR/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $ISDIR/lib64/ld-lsb-x86-64.so.3
     ;;
 esac
 patch -Np1 -i ../glibc-2.39-fhs-1.patch
@@ -90,15 +93,15 @@ cd       build
 echo "rootsbindir=/usr/sbin" > configparms
 ../configure                             \
       --prefix=/usr                      \
-      --host=$LFS_TGT                    \
+      --host=$(uname -m)-ynjun-linux-gnu                    \
       --build=$(../scripts/config.guess) \
       --enable-kernel=4.19               \
-      --with-headers=$LFS/usr/include    \
+      --with-headers=$ISDIR/usr/include    \
       --disable-nscd                     \
       libc_cv_slibdir=/usr/lib
 make
-make DESTDIR=$LFS install
-sed '/RTLDLIST=/s@/usr@@g' -i $LFS/usr/bin/ldd
+make DESTDIR=$ISDIRinstall
+sed '/RTLDLIST=/s@/usr@@g' -i $ISDIR/usr/bin/ldd
 cd ../..
 rm -rf glibc-2.39
 
@@ -108,16 +111,16 @@ cd gcc-14.1.0
 mkdir -v build
 cd       build
 ../libstdc++-v3/configure           \
-    --host=$LFS_TGT                 \
+    --host=$(uname -m)-ynjun-linux-gnu                 \
     --build=$(../config.guess)      \
     --prefix=/usr                   \
     --disable-multilib              \
     --disable-nls                   \
     --disable-libstdcxx-pch         \
-    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/14.1.0
+    --with-gxx-include-dir=/tools/$(uname -m)-ynjun-linux-gnu/include/c++/14.1.0
 make
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/lib{stdc++{,exp,fs},supc++}.la
+make DESTDIR=$ISDIRinstall
+rm -v $ISDIR/usr/lib/lib{stdc++{,exp,fs},supc++}.la
 cd ../..
 rm -rf gcc-14.1.0
 
@@ -153,10 +156,10 @@ popd
             --without-ada                \
             --disable-stripping
 make
-make DESTDIR=$LFS TIC_PATH=$(pwd)/build/progs/tic install
-ln -sv libncursesw.so $LFS/usr/lib/libncurses.so
+make DESTDIR=$ISDIRTIC_PATH=$(pwd)/build/progs/tic install
+ln -sv libncursesw.so $ISDIR/usr/lib/libncurses.so
 sed -e 's/^#if.*XOPEN.*$/#if 1/' \
-    -i $LFS/usr/include/curses.h
+    -i $ISDIR/usr/include/curses.h
 cd ..
 rm -rf ncurses-6.5
 
@@ -168,7 +171,7 @@ cd bash-5.2.21
             --without-bash-malloc              \
             bash_cv_strtold_broken=no
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 ln -sv bash $INSTALL_DIR/bin/sh
 cd ..
 rm -rf bash-5.2.21
@@ -177,16 +180,16 @@ rm -rf bash-5.2.21
 tar -xf coreutils-9.5.tar.xz
 cd coreutils-9.5
 ./configure --prefix=/usr                     \
-            --host=$LFS_TGT                   \
+            --host=$(uname -m)-ynjun-linux-gnu                   \
             --build=$(build-aux/config.guess) \
             --enable-install-program=hostname \
             --enable-no-install-program=kill,uptime
 make
-make DESTDIR=$LFS install
-mv -v $LFS/usr/bin/chroot              $LFS/usr/sbin
-mkdir -pv $LFS/usr/share/man/man8
-mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
-sed -i 's/"1"/"8"/'                    $LFS/usr/share/man/man8/chroot.8
+make DESTDIR=$ISDIRinstall
+mv -v $ISDIR/usr/bin/chroot              $ISDIR/usr/sbin
+mkdir -pv $ISDIR/usr/share/man/man8
+mv -v $ISDIR/usr/share/man/man1/chroot.1 $ISDIR/usr/share/man/man8/chroot.8
+sed -i 's/"1"/"8"/'                    $ISDIR/usr/share/man/man8/chroot.8
 cd ..
 rm -rf coreutils-9.5
 
@@ -194,10 +197,10 @@ rm -rf coreutils-9.5
 tar -xf diffutils-3.10.tar.xz
 cd diffutils-3.10
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(./build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf diffutils-3.10
 
@@ -212,10 +215,10 @@ pushd build
                --disable-zlib
   make
 popd
-./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
+./configure --prefix=/usr --host=$(uname -m)-ynjun-linux-gnu --build=$(./config.guess)
 make FILE_COMPILE=$(pwd)/build/src/file
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/libmagic.la
+make DESTDIR=$ISDIRinstall
+rm -v $ISDIR/usr/lib/libmagic.la
 cd ..
 rm -rf file-5.45
 
@@ -224,10 +227,10 @@ tar -xf findutils-4.10.0.tar.xz
 cd findutils-4.10.0
 ./configure --prefix=/usr                   \
             --localstatedir=/var/lib/locate \
-            --host=$LFS_TGT                 \
+            --host=$(uname -m)-ynjun-linux-gnu                 \
             --build=$(build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf findutils-4.10.0
 
@@ -236,10 +239,10 @@ tar -xf gawk-5.3.0.tar.xz
 cd gawk-5.3.0
 sed -i 's/extras//' Makefile.in
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf gawk-5.3.0
 
@@ -247,19 +250,19 @@ rm -rf gawk-5.3.0
 tar -xf grep-3.11.tar.xz
 cd grep-3.11
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(./build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf grep-3.11
 
 #gzip
 tar -xf gzip-1.13.tar.xz
 cd gzip-1.13
-./configure --prefix=/usr --host=$LFS_TGT
+./configure --prefix=/usr --host=$(uname -m)-ynjun-linux-gnu
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf gzip-1.13
 
@@ -268,10 +271,10 @@ tar -xf make-4.4.1.tar.gz
 cd make-4.4.1
 ./configure --prefix=/usr   \
             --without-guile \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf make-4.4.1
 
@@ -279,10 +282,10 @@ rm -rf make-4.4.1
 tar -xf patch-2.7.6.tar.xz
 cd patch-2.7.6
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf patch-2.7.6
 
@@ -290,10 +293,10 @@ rm -rf patch-2.7.6
 tar -xf sed-4.9.tar.xz
 cd sed-4.9
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(./build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf sed-4.9
 
@@ -301,10 +304,10 @@ rm -rf sed-4.9
 tar -xf tar-1.35.tar.xz
 cd tar-1.35
 ./configure --prefix=/usr   \
-            --host=$LFS_TGT \
+            --host=$(uname -m)-ynjun-linux-gnu \
             --build=$(./build-aux/config.guess)
 make
-make DESTDIR=$LFS install
+make DESTDIR=$ISDIRinstall
 cd ..
 rm -rf tar-1.35
 
@@ -312,13 +315,13 @@ rm -rf tar-1.35
 tar -xf xz-5.6.2.tar.xz
 cd xz-5.6.2
 ./configure --prefix=/usr                     \
-            --host=$LFS_TGT                   \
+            --host=$(uname -m)-ynjun-linux-gnu                   \
             --build=$(build-aux/config.guess) \
             --disable-static                  \
             --docdir=/usr/share/doc/xz-5.6.2
 make
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/liblzma.la
+make DESTDIR=$ISDIRinstall
+rm -v $ISDIR/usr/lib/liblzma.la
 cd ..
 rm -rf xz-5.6.2
 
@@ -331,7 +334,7 @@ cd       build
 ../configure                   \
     --prefix=/usr              \
     --build=$(../config.guess) \
-    --host=$LFS_TGT            \
+    --host=$(uname -m)-ynjun-linux-gnu            \
     --disable-nls              \
     --enable-shared            \
     --enable-gprofng=no        \
@@ -340,8 +343,8 @@ cd       build
     --enable-new-dtags         \
     --enable-default-hash-style=gnu
 make
-make DESTDIR=$LFS install
-rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
+make DESTDIR=$ISDIRinstall
+rm -v $ISDIR/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
 cd ../..
 rm -rf binutils-2.42
 
@@ -366,11 +369,11 @@ mkdir -v build
 cd       build
 ../configure                                       \
     --build=$(../config.guess)                     \
-    --host=$LFS_TGT                                \
-    --target=$LFS_TGT                              \
-    LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc      \
+    --host=$(uname -m)-ynjun-linux-gnu                                \
+    --target=$(uname -m)-ynjun-linux-gnu                              \
+    LDFLAGS_FOR_TARGET=-L$PWD/$(uname -m)-ynjun-linux-gnu/libgcc      \
     --prefix=/usr                                  \
-    --with-build-sysroot=$LFS                      \
+    --with-build-sysroot=$ISDIR                     \
     --enable-default-pie                           \
     --enable-default-ssp                           \
     --disable-nls                                  \
@@ -383,32 +386,32 @@ cd       build
     --disable-libvtv                               \
     --enable-languages=c,c++
 make
-make DESTDIR=$LFS install
-ln -sv gcc $LFS/usr/bin/cc
+make DESTDIR=$ISDIRinstall
+ln -sv gcc $ISDIR/usr/bin/cc
 cd ../..
 rm -rf gcc-14.1.0
 
 # chown
-chown --from lfs -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
+chown --from lfs -R root:root $ISDIR/{usr,lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
-  x86_64) chown --from lfs -R root:root $LFS/lib64 ;;
+  x86_64) chown --from lfs -R root:root $ISDIR/lib64 ;;
 esac
 
 # mount syspartition
-mkdir -pv $LFS/{dev,proc,sys,run}
-mount -v --bind /dev $LFS/dev
-mount -vt devpts devpts -o gid=5,mode=0620 $LFS/dev/pts
-mount -vt proc proc $LFS/proc
-mount -vt sysfs sysfs $LFS/sys
-mount -vt tmpfs tmpfs $LFS/run
-if [ -h $LFS/dev/shm ]; then
-  install -v -d -m 1777 $LFS$(realpath /dev/shm)
+mkdir -pv $ISDIR/{dev,proc,sys,run}
+mount -v --bind /dev $ISDIR/dev
+mount -vt devpts devpts -o gid=5,mode=0620 $ISDIR/dev/pts
+mount -vt proc proc $ISDIR/proc
+mount -vt sysfs sysfs $ISDIR/sys
+mount -vt tmpfs tmpfs $ISDIR/run
+if [ -h $ISDIR/dev/shm ]; then
+  install -v -d -m 1777 $ISDIR$(realpath /dev/shm)
 else
-  mount -vt tmpfs -o nosuid,nodev tmpfs $LFS/dev/shm
+  mount -vt tmpfs -o nosuid,nodev tmpfs $ISDIR/dev/shm
 fi
 
 # change root
-chroot "$LFS" /usr/bin/env -i   \
+chroot "$ISDIR" /usr/bin/env -i   \
     HOME=/root                  \
     TERM="$TERM"                \
     PS1='(lfs chroot) \u:\w\$ ' \
